0e60a0e097d5786592ee6b4aa8a7c72d0afd3d3b
Mekael Turner <mistnpeace@gmail.com>
2022-09-07 01:25:35 +0800

    Switch to ox-hugo configuration

--- build-site.el
+++ build-site.el
@@ -1,27 +1,28 @@
-;; Set the package installation directory so that packages aren't stored in the
+;;; Set the package installation directory so that packages aren't stored in the
 ;; ~/.emacs.d/elpa path.
 (require 'package)
 (setq package-user-dir (expand-file-name "./.packages"))
 (setq package-archives '(("melpa" . "https://melpa.org/packages/")
                          ("elpa" . "https://elpa.gnu.org/packages/")))
+;;; This is a section
 ;; Initialize the package system
 (package-initialize)
 (unless package-archive-contents
   (package-refresh-contents))
 
-;; Install html export dependencies
+;;; Install html export dependencies
 (package-install 'htmlize)
 
-;; Load the publishing system
+;;; Load the publishing system
 (require 'ox-publish)
 
-;; Customize the HTML output
+;;; Customize the HTML output
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
       org-html-head "<link rel=\"stylesheet\" href=\"https://cdn.simplecss.org/simple.min.css\" />")
 
-;; Define the publishing project
+;;; Define the publishing project
 (setq org-publish-project-alist
       (list
        (list "org-mode-site"
@@ -30,11 +31,11 @@
              :publishing-function 'org-html-publish-to-html
              :publishing-directory "./public"
              :with-author nil          ;; Don't include author name
-             :with-creator nil           ;; Include Emacs and Org versions in footer
-             :with-toc nil               ;; Include a table of contents
+             :with-creator nil           ;; Don't include Emacs and Org versions in footer
+             :with-toc nil               ;; Don't include a table of contents
              :section-numbers nil      ;; Don't include section numbers
              :time-stamp-file nil)))
 
-;; Generate the site output
+;;; Generate the site output
 (org-publish-all t)
 (message "Build complete!!")
